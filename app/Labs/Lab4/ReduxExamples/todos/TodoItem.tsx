feb02253c7778ecec5ac1d10406b21611f724310
hammer-hao <zihehaomichael@gmail.com>
2025-11-10 22:59:44 -0500

    fix todo

--- app/Labs/Lab4/ReduxExamples/todos/TodoItem.tsx
+++ app/Labs/Lab4/ReduxExamples/todos/TodoItem.tsx
@@ -3,7 +3,13 @@ import { useDispatch } from "react-redux";
 import { deleteTodo, setTodo } from "./todosReducer";
 import { Button, ListGroupItem } from "react-bootstrap";
 
-export default function TodoItem({ todo }) {
+type Todo = {
+    id: string
+    title: string
+    description: string
+}
+
+export default function TodoItem({ todo }: {todo: Todo}) {
   const dispatch = useDispatch();
 
   return (
